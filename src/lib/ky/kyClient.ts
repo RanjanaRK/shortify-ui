bd1f858b6657a4e7fe55ee55fab4510a423a39ed
RanjanaRK <ranjanakumari.me@gmail.com>
2026-01-30 19:47:55 +0530

    Update kyClient.ts

--- src/lib/ky/kyClient.ts
+++ src/lib/ky/kyClient.ts
@@ -6,19 +6,4 @@ export const kyClient = ky.create({
   mode: "cors",
   cache: "no-store",
   retry: 0,
-  hooks: {
-    beforeRetry: [
-      async ({ error, retryCount }) => {
-        if (
-          error instanceof HTTPError &&
-          error.response?.status === 401 &&
-          retryCount === 0
-        ) {
-          await ky.post(`${process.env.NEXT_PUBLIC_API_URL}/auth/refresh`, {
-            credentials: "include",
-          });
-        }
-      },
-    ],
-  },
 });
