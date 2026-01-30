bacac28d6cb0207c6d7d6ceed61001090acbe9b8
RanjanaRK <ranjanakumari.me@gmail.com>
2026-01-30 21:34:01 +0530

    Update ActivityClient.tsx

--- src/components/ActivityClient.tsx
+++ src/components/ActivityClient.tsx
@@ -1,12 +1,11 @@
 "use client ";
 
-import { getUserUrlLinks } from "@/lib/api/user.server";
-import UserLinks from "./UserLinks";
-import { GetUrlsResponse } from "@/lib/types";
 import { kyClient } from "@/lib/ky/kyClient";
+import { GetUrlsResponse } from "@/lib/types";
 import { useEffect, useState } from "react";
+import UserLinks from "./UserLinks";
 
-const ActivityClient = async () => {
+const ActivityClient = () => {
   const [urls, setUrls] = useState<GetUrlsResponse | null>(null);
   const [loading, setLoading] = useState(true);
 
