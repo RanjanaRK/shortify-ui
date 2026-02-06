c8c64024e8382fde75f457d73a8831dc75c4c1a2
RanjanaRK <ranjanakumari.me@gmail.com>
2026-02-06 23:03:04 +0530

    Update Footer.tsx

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <>
-      <footer className="bg-sky-950 text-sky-100">
+      <footer className="border-t bg-sky-50 text-sky-900">
         <div className="mx-auto flex max-w-7xl items-center justify-between px-6 py-6 text-sm">
           <p>&copy;{new Date().getFullYear()} ShortiFy</p>
 
